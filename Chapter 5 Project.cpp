75f30789e51a49db2be3b40020f55c12e4efdc0c
Malakye Brown <prophetoftruth07@gmail.com>
2025-11-06 16:02:31 -0500

    Code Complete, Worry about Wording

--- Chapter 5 Project.cpp
+++ Chapter 5 Project.cpp
@@ -1,9 +1,35 @@
 // This is the Population Chart Subproject
 
 #include <iostream>
+#include <fstream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    cout << "This is the PC subproject branch!\n";
+    int year = 1880;
+    int asteriskLoopStart = 0;
+    int population, asteriskLoopEnd;
+
+    ifstream inFile("People.txt");
+    if (!inFile) {
+        cout << "File Opening Error!" << endl;
+        return 1;
+    }
+
+
+
+
+
+    while (inFile >> population) {
+        asteriskLoopEnd = population / 1000;
+        year += 20;
+        cout << year << "  ";
+        do {
+            cout << "*";
+            ++asteriskLoopStart;
+        } while (asteriskLoopStart != asteriskLoopEnd);
+        cout << "\n";
+        asteriskLoopStart = 0;
+    }
 }
