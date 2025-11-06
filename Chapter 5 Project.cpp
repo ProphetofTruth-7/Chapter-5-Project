cf9231bc285a6cfa9a97b73c4e5934f0f89d9fcd
Malakye Brown <prophetoftruth07@gmail.com>
2025-11-06 12:56:04 -0500

    Finished

--- Chapter 5 Project.cpp
+++ Chapter 5 Project.cpp
@@ -5,5 +5,29 @@ using namespace std;
 
 int main()
 {
-    cout << "This is the pattern branch!\n";
+	int levelCounter = 1;
+	int plusCounter = 0;
+	do {
+		while (plusCounter != levelCounter) {
+			++plusCounter;
+			cout << "+";
+		}
+		cout << "\n" << endl;
+		++levelCounter;
+		plusCounter = 0;
+	} while (levelCounter != 11);
+
+	cout << "\n" << endl;
+	levelCounter = 1;
+	plusCounter = 11;
+
+	do {
+		while (plusCounter != levelCounter) {
+			cout << "+";
+			--plusCounter;
+		}
+		cout << "\n" << endl;
+		++levelCounter;
+		plusCounter = 11;
+	} while (levelCounter != 11);
 }
