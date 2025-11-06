c54f566a6149700242e12abdeb87b137e7b0d5ce
Malakye Brown <prophetoftruth07@gmail.com>
2025-11-06 12:57:39 -0500

    Establishment

--- Chapter 5 Project.cpp
+++ Chapter 5 Project.cpp
@@ -1,9 +1,9 @@
-// This is the Main Branch
+// This is the Lineup Subproejct
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    cout << "This is the main branch!\n";
+    cout << "This is the lineup subproject!\n";
 }
